3fe563d985fe63267f04dd99b2d68c5692ccc01f
Holger Brunn <hbrunn@therp.nl>
2013-10-04 18:11:32 +0200

    [ADD] icon [IMP] description

--- fts_document/fts_document.py
+++ fts_document/fts_document.py
@@ -19,10 +19,7 @@
 #
 ##############################################################################
 
-try:
-    from openerp.addons.fts_base.fts_base import fts_base
-except:
-    from fts_base.fts_base import fts_base
+from openerp.addons.fts_base.fts_base import fts_base
 
 class fts_document(fts_base):
 
