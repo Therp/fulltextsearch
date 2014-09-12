3240246cce85af09da23200e3203c64c9609800d
Holger Brunn <hbrunn@therp.nl>
2014-09-12 14:17:39 +0200

    [FIX] pep8

--- fts_base/fts_proxy.py
+++ fts_base/fts_proxy.py
@@ -33,6 +33,7 @@ import psycopg2
 import logging
 logger = logging.getLogger('fulltextsearch')
 
+
 class fts_proxy(TransientModel):
 
     _name = 'fts.proxy'
@@ -41,22 +42,22 @@ class fts_proxy(TransientModel):
         self.read(cr, uid, ids, ['model'])
 
     _columns = {
-              'name': fields.char('Name', size=256, readonly=True),
-              'text': fields.function(lambda *args,**kwargs: {}, string='Searchstring',
-                                      type='text',
-                                      fnct_search=lambda *args,**kwargs: {}, readonly=True),
-              'model': fields.char('Model', size=256, readonly=True),
-              'model_name': fields.function(lambda self, cr, uid, ids, name, arg, context: 
-                  dict([
-                      (this['id'], self.pool.get('ir.model').name_search(
-                          cr, uid, this['model'])[0][1]) 
-                      for this in 
-                      self.read(cr, uid, ids, ['model'])]), string='Type', 
-                  type='char', translate=True, readonly=True),
-              'res_id': fields.integer('Res ID', readonly=True),
-              'rank': fields.float('Rank', readonly=True),
-              'summary': fields.text('Summary', readonly=True)
-              }
+        'name': fields.char('Name', size=256, readonly=True),
+        'text': fields.function(
+            lambda *args, **kwargs: {}, string='Searchstring', type='text',
+            fnct_search=lambda *args, **kwargs: {}, readonly=True),
+        'model': fields.char('Model', size=256, readonly=True),
+        'model_name': fields.function(
+            lambda self, cr, uid, ids, name, arg, context: dict([
+                (this['id'], self.pool.get('ir.model').name_search(
+                    cr, uid, this['model'])[0][1])
+                for this in
+                self.read(cr, uid, ids, ['model'])]),
+            string='Type',  type='char', translate=True, readonly=True),
+        'res_id': fields.integer('Res ID', readonly=True),
+        'rank': fields.float('Rank', readonly=True),
+        'summary': fields.text('Summary', readonly=True)
+    }
 
     _order = 'rank DESC, model ASC'
 
@@ -70,7 +71,7 @@ class fts_proxy(TransientModel):
         fts_base.pool = pool
         for plugin_type in fts_base_meta._to_register:
             plugin_module = plugin_type.__module__.split('.', 3)[2]
-            if not plugin_module in pool._init_modules:
+            if plugin_module not in pool._init_modules:
                 continue
             if plugin_type in fts_base._plugins:
                 continue
@@ -83,7 +84,7 @@ class fts_proxy(TransientModel):
                context=None, count=False):
 
         if context is None:
-            context={}
+            context = {}
 
         searchstring = ''
         models = []
@@ -102,41 +103,45 @@ class fts_proxy(TransientModel):
 
         if not context.get('fts_no_syntax_check') and not offset and not count:
             try:
-                cr.execute("select to_tsquery('pg_catalog.simple', %s)", (searchstring,))
+                cr.execute("select to_tsquery('pg_catalog.simple', %s)",
+                           (searchstring,))
             except psycopg2.ProgrammingError:
-                raise except_orm(_('Error'), 
-                        _('You filled in an invalid searchstring!'))
+                raise except_orm(
+                    _('Error'), _('You filled in an invalid searchstring!'))
 
-        #TODO: if this search is limited, it is probably about scrolling and 
-        #we have cached the results of the initial nonlimited search. So return 
-        #that. Should look something like
-        #if self.search.lookup(self, cr, args, 0, None, order, context, count))
-        #and works only in openerp6.1
+        # TODO: if this search is limited, it is probably about scrolling and
+        # we have cached the results of the initial nonlimited search. So
+        # return that. Should look something like
+        # if self.search.lookup(self, cr, args, 0, None, order, context,
+        #                       count))
+        # and works only in openerp6.1
 
-        logger.debug('offset: %s limit: %s order=%s count=%s' % (offset, limit,
-            order, count))
+        logger.debug(
+            'offset: %s limit: %s order=%s count=%s' % (offset, limit,
+                                                        order, count)
+        )
         logger.debug('args ' + str(args))
         logger.debug('context ' + str(context))
 
-        #TODO: context may contain info which document types are interesting
-        #only call search for that ones in this case
+        # TODO: context may contain info which document types are interesting
+        # only call search for that ones in this case
         for search_plugin in fts_base_meta._plugins:
 
             if models and search_plugin._model not in models:
                 continue
 
             logger.debug('plugin ' + str(search_plugin))
-            res += search_plugin.search(cr, uid, args, order=order,
-                                            context=context, count=count,
-                                            searchstring=searchstring)
+            res += search_plugin.search(
+                cr, uid, args, order=order, context=context, count=count,
+                searchstring=searchstring)
             logger.debug(res)
             logger.debug('finished')
 
         if count:
             return res
 
-        #TODO: cache ids of results for scolling (offset > 0). that should use
-        #some kind of hash over the search parameters
+        # TODO: cache ids of results for scolling (offset > 0). that should use
+        # some kind of hash over the search parameters
         return super(fts_proxy, self).search(cr, uid, [('id', 'in', res)],
                                              offset=offset,
                                              limit=limit, order=order,
@@ -146,11 +151,12 @@ class fts_proxy(TransientModel):
 
         fts_classname = (fts_object._model)
 
-        self.pool.get('ir.cron').create(cr, SUPERUSER_ID,
+        self.pool.get('ir.cron').create(
+            cr, SUPERUSER_ID,
             {
                 'name': 'fulltextsearch init ' + fts_classname,
-                'nextcall' : datetime.now().strftime(
-                                                DEFAULT_SERVER_DATETIME_FORMAT),
+                'nextcall': datetime.now().strftime(
+                    DEFAULT_SERVER_DATETIME_FORMAT),
                 'model': self._name,
                 'function': 'init_tsvector_cronjob',
                 'args': "('" + fts_classname + "',)",
@@ -161,8 +167,8 @@ class fts_proxy(TransientModel):
     def recreate_search_index(self, cr, uid, fts_object):
         fts_object._drop_indexed_column_trigger(self.pool, cr)
         fts_object._create_indexed_column_trigger(self.pool, cr)
-        self.pool.get('fts.proxy').create_init_tsvector_cronjob(cr, 
-                uid, fts_object)
+        self.pool.get('fts.proxy').create_init_tsvector_cronjob(
+            cr, uid, fts_object)
 
     def init_tsvector_cronjob(self, cr, uid, fts_classname, context=None):
 
@@ -170,7 +176,8 @@ class fts_proxy(TransientModel):
 
         for search_plugin in fts_base_meta._plugins:
             if (search_plugin._model) == fts_classname:
-
-                logger.info('running _init_tsvector_column for '+fts_classname)
+                logger.info(
+                    'running _init_tsvector_column for ' + fts_classname)
                 search_plugin._init_tsvector_column(self.pool, cr)
-                logger.info('finished _init_tsvector_column for '+fts_classname)
+                logger.info(
+                    'finished _init_tsvector_column for ' + fts_classname)
