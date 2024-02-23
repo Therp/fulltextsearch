c745e7b4059806d4a52b519f43f2e30c0b21ac77
Ronald Portier <ronald@therp.nl>
2024-02-23 18:24:18 +0100

    [MIG] Migration or rather reimplementation on 16.0

---
+++ fts_base/README.md
@@ -0,0 +1,3 @@
+# fts_base
+
+Base to add FullText Search to Odoo models.
--- fts_base/models/__init__.py
+++ fts_base/models/__init__.py
@@ -1,5 +1,4 @@
 # Copyright 2012-2024 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from . import models
-
-# from . import wizards
+from . import fts_mixin
+from . import fts_proxy
