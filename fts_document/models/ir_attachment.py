a9657cdfae33e59372c423c9f97df7154d3e81b3
Ronald Portier <ronald@therp.nl>
2024-03-20 17:52:07 +0100

    [IMP] fts_document: exclude internal attachments from search

--- fts_document/models/ir_attachment.py
+++ fts_document/models/ir_attachment.py
@@ -1,6 +1,7 @@
 # Copyright 2012-2024 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models
+from odoo import api, models
+from odoo.osv.expression import AND
 
 from odoo.addons.fts_base.tsvector_field import TSVector
 
@@ -19,3 +20,9 @@ class IrAttachment(models.Model):
         ],
         help="FT Search on content",
     )
+
+    @api.model
+    def _proxy_search(self, searchstring, domain, **kwargs):
+        """Exclude internal attachments from search."""
+        patched_domain = AND([domain, ["!", ("res_model", "=ilike", "ir.%")]])
+        return super()._proxy_search(searchstring, patched_domain, **kwargs)
