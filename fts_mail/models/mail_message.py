6958b0a5b32350799197cec824c6c3920a378fa0
Ronald Portier <ronald@therp.nl>
2025-01-28 19:32:29 +0100

    [IMP] fts_mail: also limit size of indexed mail bodies

--- fts_mail/models/mail_message.py
+++ fts_mail/models/mail_message.py
@@ -1,8 +1,10 @@
-# Copyright 2012-2024 Therp BV <https://therp.nl>.
+# Copyright 2012-2025 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import models
 
 from odoo.addons.fts_base.tsvector_field import TSVector
+
+INDEX_CONTENT_TSVECTOR = "SUBSTRING(COALESCE(body, ''::text), 1, 512 * 1024)"
 
 
 class MailMessage(models.Model):
@@ -15,8 +17,6 @@ class MailMessage(models.Model):
     _proxy_search_field = "body_tsvector"
 
     body_tsvector = TSVector(
-        indexed_columns=[
-            "body",
-        ],
+        indexed_columns=INDEX_CONTENT_TSVECTOR,
         help="FT Search on content",
     )
