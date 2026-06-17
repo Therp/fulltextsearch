4260cf85c8a69f35e145bdee48705b278bb72cc8
Nikos Tsirintanis <ntsirintanis@therp.nl>
2026-06-17 14:56:13 +0200

    [MIG] fts_mail: Migration to 18.0

--- fts_mail/__manifest__.py
+++ fts_mail/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Fulltext search - mails",
     "summary": "Add fulltext search on mail messages",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "depends": ["fts_base", "mail"],
     "website": "https://github.com/Therp/fulltextsearch",
     "author": "Therp BV",
